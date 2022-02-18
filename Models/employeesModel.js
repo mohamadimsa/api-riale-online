56b429a2ee3d4dcccaea61bbc83f15d289a46a88
mohamadi msa <mohamadi1.msa-abdou@epitech.eu>
2022-02-18 16:02:05 +0100

    creation du model employee model et de la methode createEmplyee

---
+++ Models/employeesModel.js
@@ -0,0 +1,18 @@
+const db = require("$db");
+
+/**cette fonction nous permet de cree un employer
+ * @param {objet} data
+ * @returns {Promise}
+ */
+const createEmployee = async (data) => {
+  try {
+    return await db.employees.create()({ data: data });
+  } catch (error) {
+    console.log(error.toString());
+    return false;
+  }
+};
+
+module.exports = {
+  createEmployee,
+};

e22061a10abab8e5b3675887d13168adf74b9ae1
mohamadi msa <mohamadi1.msa-abdou@epitech.eu>
2022-02-18 15:48:17 +0100

    creation de la route permettant de cree un office

---
+++ routes/office.js
@@ -0,0 +1,18 @@
+const Controller = require("$controllers");
+
+module.exports = [
+  /**
+   * cette route nous permet de cree un office
+   * @Route : /office
+   * @Method : post
+   */
+  {
+    url: "/office",
+    method: "post",
+    func: [Controller["OfficeController@create"]],
+    functionName: "create",
+    perm: ['BANK_ADMIN']
+    
+  },
+
+];

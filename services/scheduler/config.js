bb5a11b4aab924b187a2fc1b1bcffc5d63ca7a63
Mohamadi msa <73989090+mohamadimsa@users.noreply.github.com>
2022-04-20 18:48:40 +0200

    mise en place des commentaire

--- services/scheduler/config.js
+++ services/scheduler/config.js
@@ -1,3 +1,8 @@
+/**
+ * liste des tache cron programmer
+ * frequency : la frequence de la tache
+ * handler : fonction a executer
+ */
 module.exports = {
   hello: {
     frequency: "*/1 * * * * *",
--- services/scheduler/scheduler.js
+++ services/scheduler/scheduler.js
@@ -1,17 +1,22 @@
 const cron = require("node-cron");
 const { resolve } = require("path");
 
+/**
+ * cette fonction nous permet de charger toute les tache crond configurer dans le ficher config
+ */
 module.exports = {
   initCrons: (config) => {
     Object.keys(config).forEach((key) => {
       if (cron.validate(config[key].frequency)) {
         cron.schedule(config[key].frequency, () => {
-          const handler = require(resolve("services","scheduler",config[key].handler));
-          handler()
+          const handler = require(resolve(
+            "services",
+            "scheduler",
+            config[key].handler
+          ));
+          handler();
         });
       }
     });
   },
 };
-
-
